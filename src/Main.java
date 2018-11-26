c901802e2590f1461581d9a20201106053142042
Marat_Gabdrakhmanov <gabdra-marat@yandex.ru>
2018-11-26 12:11:14 +0400

    j

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import Xml.Unmarshalling;
 
 public class Main   {
 
-    private static final String XMLpath = "C:\\Users\\User\\IdeaProjects\\XMLParsier\\games.xml";
+    private static final String XMLpath = "C:\\Users\\Marat_Gabdrakhmanov\\IdeaProjects\\XMLParsier1\\games.xml";
     public static void main(String[] args) throws Exception {
 
         Parser parse = new Parser();
